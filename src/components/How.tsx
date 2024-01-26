cf55f98d17cf4c99363d7ce9609a414f8eef2a16
Byakkuya <contact.ahmedzbaa@gmail.com>
2024-01-26 11:45:38 +0100

    how component

--- src/components/How.tsx
+++ src/components/How.tsx
@@ -2,8 +2,57 @@ import React from 'react';
 
 const How = () => {
     return (
-        <div>
-            <h1>How</h1>
+        <div className="h-screen ">
+            <div className="md:w-3/5 mx-auto">
+                <h2 className="text-4xl text-neutral-700 font-semibold mb-4 md:w-4/5">As a Developer you can ....</h2>
+                <p className="md:w-3/4 text-sm text-neutral-600 mb-8">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis metus rhoncus, dapibus nibh
+                    sagittis, posuere magna. In hac habitasse platea dictumst. Aliquam sodales eu eros non maximus.
+                    Nulla tincidunt eu risus eu pellentesque. Duis a leo nec nisl molestie sollicitudin
+                </p>
+            </div>
+
+            <div className="max-w-7xl mx-auto w-full grid grid-cols-9 px-2">
+
+                <div className="col-span-4 w-full h-full ">
+                    <div className="w-full h-full bg-[rgba(255,,255,0.04)] shadow-3xl rounded-md p-2 md:pl-4">
+                        <h1 className="text-black text-xl font-medium py-2">Make an Account</h1>
+                        <p className="text-gray-600 sm:text-sm text-xs">create an account  or just login if your are aldready have an account </p>
+                    </div>
+                </div>
+                <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                    <div className="h-full w-1 bg-indigo-300"></div>
+                    <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center">1</div>
+                </div>
+                <div className="col-span-4 w-full h-full"></div>
+
+
+
+                <div className="col-span-4 w-full h-full"></div>
+                <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                    <div className="h-full w-1 bg-indigo-300"></div>
+                    <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center">2</div>
+                </div>
+                <div className="col-span-4 w-full h-full ">
+                    <div className="w-full h-full bg-[rgba(255,,255,0.04)] shadow-3xl rounded-md p-2 md:pl-4">
+                        <h1 className="text-black text-xl font-medium py-2">Fill your information</h1>
+                        <p className="text-gray-600 sm:text-sm text-xs">depends on your Account there is a form to fill including your details </p>
+                    </div>
+                </div>
+
+
+                <div className="col-span-4 w-full h-full ">
+                    <div className="w-full h-full bg-[rgba(255,,255,0.04)] shadow-3xl rounded-md p-2 md:pl-4">
+                        <h1 className="text-black text-xl font-medium py-2">Start Your Journey</h1>
+                        <p className="text-gray-600 sm:text-sm text-xs">Depends on your Account you can now search an apply to jobs as A Developer or post Offers for Developers as an Entreprise </p>
+                    </div>
+                </div>
+                <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                    <div className="h-full w-1 bg-indigo-300"></div>
+                    <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center">3</div>
+                </div>
+                <div className="col-span-4 w-full h-full"></div>
+            </div>
         </div>
     );
 };
