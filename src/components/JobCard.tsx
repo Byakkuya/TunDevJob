5a73c7d9085ab3318df2a2721b08822503b409dd
Byakkuya <contact.ahmedzbaa@gmail.com>
2024-02-06 16:17:42 +0100

    Revert "adjust the the JobCard and CompanyCard"
    This reverts commit df2eccd5096f34e48859455e5136ba2d39293724.

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -10,32 +10,39 @@ interface JobCardProps {
 const JobCard: React.FC<JobCardProps> = ({ job }) => {
     return (
         <Link to={`/job-detail/${job?.id}`}>
-            <div className="w-full md:w-[16rem] 2xl:w-[18rem] h-[16rem] md:h-[18rem] bg-white flex flex-col justify-between shadow-lg rounded-lg px-3 py-5 transition-transform transform hover:scale-105">
-                <div className="flex gap-3">
+            <div
+                className='w-full md:w-[16rem] 2xl:w-[18rem] h-[16rem] md:h-[18rem] bg-white flex flex-col justify-between shadow-lg
+                rounded-md px-3 py-5 '
+            >
+                <div className='flex gap-3'>
                     <img
                         src={job?.Company?.profileUrl} // Assuming profileUrl is in your Company type
                         alt={job?.Company?.name}
-                        className="w-14 h-14 rounded-full"
+                        className='w-14 h-14'
                     />
 
-                    <div className="flex flex-col">
-                        <p className="text-lg font-semibold truncate">{job?.jobTitle}</p>
-                        <span className="flex items-center gap-2 text-gray-500 text-sm">
-              <GoLocation className="text-slate-900" />
+                    <div className=''>
+                        <p className='text-lg font-semibold truncate'>{job?.jobTitle}</p>
+                        <span className='flex gap-2 items-center'>
+              <GoLocation className='text-slate-900 text-sm' />
                             {job?.location}
             </span>
                     </div>
                 </div>
 
-                <div className="py-3">
-                    <p className="text-sm">{job?.detail[0]?.description?.slice(0, 150) + "..."}</p>
+                <div className='py-3'>
+                    <p className='text-sm'>
+                        {job?.detail[0]?.description?.slice(0, 150) + "..."}
+                    </p>
                 </div>
 
-                <div className="flex items-center justify-between">
-                    <p className="bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm">
+                <div className='flex items-center justify-between'>
+                    <p className='bg-[#1d4fd826] text-[#1d4fd8] py-0.5 px-1.5 rounded font-semibold text-sm'>
                         {job?.jobType}
                     </p>
-                    <span className="text-gray-500 text-sm">{moment(job?.created_at).fromNow()}</span>
+                       <span className='text-gray-500 text-sm'>
+            {moment(job?.created_at).fromNow()}
+          </span>
                 </div>
             </div>
         </Link>
