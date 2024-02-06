df2eccd5096f34e48859455e5136ba2d39293724
Byakkuya <contact.ahmedzbaa@gmail.com>
2024-02-06 15:32:54 +0100

    adjust the the JobCard and CompanyCard

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -8,34 +8,33 @@ interface CompanyCardProps {
 
 const CompanyCard: React.FC<CompanyCardProps> = ({ cmp }) => {
     return (
-        <div className='w-full h-16 flex gap-4 items-center justify-between bg-white shadow-md rounded'>
-            <div className='w-3/4 md:w-2/4 flex gap-4 items-center'>
+        <div className="w-full h-16 flex gap-4 items-center justify-between bg-white shadow-md rounded-md p-4 transition-transform transform hover:scale-105">
+            <div className="w-3/4 md:w-2/4 flex gap-4 items-center">
                 <Link to={`/company-profile/${cmp?._id}`}>
                     <img
                         src={cmp?.profileUrl}
                         alt={cmp?.name}
-                        className='w-8 md:w-12 h-8 md:h-12 rounded'
+                        className="w-12 h-12 rounded-full"
                     />
                 </Link>
-                <div className='h-full flex flex-col'>
+                <div className="flex flex-col">
                     <Link
                         to={`/company-profile/${cmp?._id}`}
-                        className='text-base md:text-lg font-semibold text-gray-600 truncate'
+                        className="text-lg font-semibold text-gray-800 hover:text-blue-600 truncate"
                     >
                         {cmp?.name}
                     </Link>
-                    <span className='text-sm text-blue-600'>{cmp?.email}</span>
+                    <span className="text-sm text-blue-600">{cmp?.email}</span>
                 </div>
             </div>
 
-            <div className='hidden w-1/4 h-full md:flex items-center'>
-                <p className='text-base text-start'>{cmp?.location}</p>
+            <div className="hidden w-1/4 h-full md:flex items-center">
+                <p className="text-base text-start text-gray-600 truncate">{cmp?.location}</p>
             </div>
 
-            <div className='w-1/4 h-full flex flex-col items-center'>
-                {/* Assuming jobPosts is a property of Company type */}
-                <p className='text-blue-600 font-semibold'>{cmp?.jobPosts?.length}</p>
-                <span className='text-xs md:base font-normal text-gray-600'>
+            <div className="w-1/4 h-full flex flex-col items-center">
+                <p className="text-blue-600 font-semibold">{cmp?.jobPosts?.length}</p>
+                <span className="text-xs md:text-sm font-normal text-gray-600">
           Jobs Posted
         </span>
             </div>
